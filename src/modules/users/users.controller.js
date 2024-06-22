8b895f6310f74a0ace88175cccc0f298beeda53b
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-06-22 18:50:32 +0300

    *-/*-*/-

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -3,8 +3,7 @@ import { userModel } from "../../../database/models/user.model.js";
 import slugify from "slugify";
 import ApiFeature from "../../utils/apiFeature.js";
 import catchAsync from "../../utils/middleWare/catchAsyncError.js";
-
-
+import AppError from "../../utils/appError.js";
 
 const createUser = catchAsync(async (req, res, next) => {
   let userExists = await userModel.findOne({ email: req.body.email });
@@ -19,50 +18,47 @@ const createUser = catchAsync(async (req, res, next) => {
   let addedUser = await newUser.save();
 
   res.status(201).json({
-    message: "User created successfully!",addedUser
+    message: "User created successfully!",
+    addedUser,
   });
 });
 
 const getAllUsers = catchAsync(async (req, res, next) => {
-let ApiFeat = new ApiFeature(userModel.find(), req.query)
-.pagination()
-.filter()
-.sort()
-.search()
-.fields();
+  let ApiFeat = new ApiFeature(userModel.find(), req.query)
+    .pagination()
+    .filter()
+    .sort()
+    .search()
+    .fields();
 
-let results = await ApiFeat.mongooseQuery;
-res.json({ message: "done", page: ApiFeat.page, results });
+  let results = await ApiFeat.mongooseQuery;
+  res.json({ message: "done", page: ApiFeat.page, results });
   if (!results) {
     return res.status(404).json({
       message: "No users was found! Create a new user to get started!",
     });
   }
-
 });
 
 const getUserById = catchAsync(async (req, res, next) => {
-  let { userId } = req.params;
+  let { id } = req.params;
 
-  let user = await userModel.findById(userId);
-
-  res.status(200).json({ user });
+  let results = await userModel.findById(id);
+  !results && next(new AppError(`not found `, 404));
+  results && res.json({ message: "done", results });
 });
 
 const updateUser = catchAsync(async (req, res, next) => {
   let { userId } = req.params;
-  let { name } = req.body;
 
-  let updatedUser = await userModel.findByIdAndUpdate(
-    userId,
-    { name, slug: slugify(name) },
-    { new: true }
-  );
+  let updatedUser = await userModel.findByIdAndUpdate(userId, req.body, {
+    new: true,
+  });
 
   if (!updatedUser) {
     return res
       .status(404)
-      .json({ message: "Couldn't update! User not found!",updatedUser });
+      .json({ message: "Couldn't update! User not found!", updatedUser });
   }
 
   res.status(200).json({ message: "User updated successfully!" });
