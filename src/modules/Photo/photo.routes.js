6d4722369f28035ae826f9c38d0da1d505734a84
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-06-27 15:38:43 +0300

    -+-+-+

--- src/modules/Photo/photo.routes.js
+++ src/modules/Photo/photo.routes.js
@@ -4,8 +4,16 @@ import { uploadSingleFile } from "../../utils/middleWare/fileUploads.js";
 
 const photoRouter = express.Router();
 
-photoRouter.post("/invoice",uploadSingleFile("photo", "image"),photoController.createIamge);
-photoRouter.post("/user",uploadSingleFile("photo", "image"),photoController.createIamge);
+photoRouter.post(
+  "/",
+  uploadSingleFile("photo", "image"),
+  photoController.createIamge
+);
+photoRouter.post(
+  "/",
+  uploadSingleFile("photo", "image"),
+  photoController.createIamge
+);
 photoRouter.get("/", photoController.getAllIamge);
 photoRouter.put("/:id", photoController.editIamge);
 photoRouter.delete("/:id", photoController.deleteIamge);
