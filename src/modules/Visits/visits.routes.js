8b05c589298ec87c2d0ff2bc8bd38b66a2bc74bb
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-07-02 13:40:04 +0300

    vv

--- src/modules/Visits/visits.routes.js
+++ src/modules/Visits/visits.routes.js
@@ -4,6 +4,7 @@ import * as visitController from "./visits.controller.js";
 const visitRouter = express.Router();
 
 visitRouter.get("/", visitController.getAllVisits);
+visitRouter.get("/user/:id", visitController.getAllVisits);
 visitRouter.post("/", visitController.createVisit);
 visitRouter.put("/:id", visitController.editVisit);
 visitRouter.delete("/:id", visitController.deleteVisit);
